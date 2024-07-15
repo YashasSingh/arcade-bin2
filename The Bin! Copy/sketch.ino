80bf72d61f029a3d56aaf1730ad841211033fc35
YashasSingh <121969113+YashasSingh@users.noreply.github.com>
2024-07-15 16:26:44 -0400

    Add files via upload

--- The Bin! Copy/sketch.ino
+++ The Bin! Copy/sketch.ino
@@ -18,6 +18,9 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 sensors_event_t accel_event, gyro_event, temp_event;
 
+float speedX = 0, speedY = 0, speedZ = 0;
+unsigned long lastTime;
+
 void setup() {
   // Start serial communication at 115200 baud rate
   Serial1.begin(115200);
@@ -34,22 +37,36 @@ void setup() {
   Serial1.println("MPU6050 initialized");
 
   // Initialize the OLED display
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
     Serial1.println(F("SSD1306 allocation failed"));
-    for (;;) ; // Don't proceed, loop forever
+    for(;;); // Don't proceed, loop forever
   }
   display.display();
   delay(2000); // Pause for 2 seconds
 
   // Clear the buffer
   display.clearDisplay();
+
+  lastTime = millis();
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+  float deltaTime = (currentTime - lastTime) / 1000.0; // deltaTime in seconds
+  lastTime = currentTime;
+
   // Update the MPU6050 sensor data
   mpu.getAccelerometerSensor()->getEvent(&accel_event);
   mpu.getGyroSensor()->getEvent(&gyro_event);
   mpu.getTemperatureSensor()->getEvent(&temp_event);
+
+  // Estimate speed by integrating acceleration
+  speedX += accel_event.acceleration.x * deltaTime;
+  speedY += accel_event.acceleration.y * deltaTime;
+  speedZ += accel_event.acceleration.z * deltaTime;
+
+  // Calculate the resultant speed
+  float speed = sqrt(speedX * speedX + speedY * speedY + speedZ * speedZ);
 
   // Read the LDR sensor value
   int ldrValue = analogRead(LDR_PIN);
@@ -57,13 +74,9 @@ void loop() {
   // Print sensor data to serial
   Serial1.print("[");
   Serial1.print(millis());
-  Serial1.print("] X: ");
-  Serial1.print(accel_event.acceleration.x);
-  Serial1.print(", Y: ");
-  Serial1.print(accel_event.acceleration.y);
-  Serial1.print(", Z: ");
-  Serial1.print(accel_event.acceleration.z);
-  Serial1.print(" m/s^2, LDR: ");
+  Serial1.print("] Speed: ");
+  Serial1.print(speed);
+  Serial1.print(" m/s, LDR: ");
   Serial1.println(ldrValue);
 
   // Display sensor data on OLED
@@ -71,16 +84,10 @@ void loop() {
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  display.println("MPU6050 Data:");
-  display.print("Accel X: ");
-  display.println(accel_event.acceleration.x);
-  display.print("Accel Y: ");
-  display.println(accel_event.acceleration.y);
-  display.print("Accel Z: ");
-  display.println(accel_event.acceleration.z);
+  display.println("Speedometer");
+  display.print("Speed: "); display.print(speed); display.println(" m/s");
   display.println();
-  display.print("LDR: ");
-  display.println(ldrValue);
+  display.print("LDR: "); display.println(ldrValue);
   display.display();
 
   delay(500);
