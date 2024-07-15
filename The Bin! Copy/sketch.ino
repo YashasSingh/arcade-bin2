6260ebf52a6f972d5099a85ebab5eb5646ae46a3
YashasSingh <121969113+YashasSingh@users.noreply.github.com>
2024-07-15 14:26:42 -0400

    Add files via upload

--- The Bin! Copy/sketch.ino
+++ The Bin! Copy/sketch.ino
@@ -4,7 +4,7 @@
 // Create an MPU6050 object
 Adafruit_MPU6050 mpu;
 
-
+sensors_event_t accel_event, gyro_event, temp_event;
 void setup() {
   // Start serial communication at 115200 baud rate
   Serial1.begin(115200);
@@ -14,23 +14,28 @@ void setup() {
   Wire.begin();
 
   // Initialize the MPU6050 sensor
-  mpu.begin();
-   // Calculates gyro offsets
-
+  while (!mpu.begin()) {
+    Serial.println("MPU6050 not connected!");
+    delay(1000);
+  }
   Serial1.println("MPU6050 initialized");
 }
 
+
 void loop() {
   // Update the MPU6050 sensor data
-  mpu.update();
+  mpu.getAccelerometerSensor()->getEvent(&accel_event);
+  mpu.getGyroSensor()->getEvent(&gyro_event);
+  mpu.getTemperatureSensor()->getEvent(&temp_event);
 
-  // Print gyroscope data
-  Serial1.print("GyroX: ");
-  Serial1.print(mpu.getGyroX());
-  Serial1.print("\tGyroY: ");
-  Serial1.print(mpu.getGyroY());
-  Serial1.print("\tGyroZ: ");
-  Serial1.println(mpu.getGyroZ());
-
-  delay(100); // Delay for 100 milliseconds
+  Serial1.print("[");
+  Serial1.print(millis());
+  Serial1.print("] X: ");
+  Serial1.print(accel_event.acceleration.x);
+  Serial1.print(", Y: ");
+  Serial1.print(accel_event.acceleration.y);
+  Serial1.print(", Z: ");
+  Serial1.print(accel_event.acceleration.z);
+  Serial1.println(" m/s^2");
+  delay(500);
 }
